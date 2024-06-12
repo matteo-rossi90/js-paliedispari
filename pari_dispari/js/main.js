46410fd53c4b184f1751fb490c7bb0db808c247e
matteo-rossi90 <matteo.rossi1314@gmail.com>
2024-06-12 15:29:05 +0200

    feat: add js code function to have a random number and to state if a number is odd or even

--- pari_dispari/js/main.js
+++ pari_dispari/js/main.js
@@ -5,3 +5,42 @@ Dichiariamo chi ha vinto*/
 
 ///////////////////////////////////////////////////////////////////////
 
+//chiedere all'utente pari o dispari
+let askUser = prompt('Pari o dispari');
+console.log(askUser);
+
+//chiedere all'utente di inserire un numero da 1 a 5
+let numUser = parseInt(prompt('Inserisci un numero da 1 a 5'));
+console.log(numUser);
+
+//fare in modo che il computer generi un numero compreso tra 1 e 5
+let numPC = randomPC(1, 5);
+console.log(numPC);
+
+
+//FUNZIONI//
+
+//generare un numero random da 1 a 5 per il computer
+function randomPC(min, max){
+
+    let num = Math.floor(Math.random() * (max - min) + min);
+
+    return num;
+
+}
+
+//capire se la somma dei due numeri darà un numero pari o un numero dispari
+function sumNumber(numUser, numPC){
+
+    //sommare i due valori
+    let sum = numUser + numPC;
+
+    if ((numUser && numPC) % 2 === 0){
+        sum = 'pari';
+    }else{
+        sum = 'dispari';
+    }
+
+    return sum;
+}
+
